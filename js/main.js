4721fa82c6af8c1016e35946c974e28105729af3
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-11-16 08:19:38 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,44 +20,5 @@ $('[data-toggle="counter-up"]').counterUp({
   delay: 10,
   time: 2000
 });
-
-
-/* FILTRO */
-/**
-   * recomend isotope and filter
-   */
-let recomendIsotope = document.querySelector('.recomend-isotope');
-
-if (recomendIsotope) {
-
-  let recomendFilter = recomendIsotope.getAttribute('data-recomend-filter') ? recomendIsotope.getAttribute('data-recomend-filter') : '*';
-  let recomendLayout = recomendIsotope.getAttribute('data-recomend-layout') ? recomendIsotope.getAttribute('data-recomend-layout') : 'masonry';
-  let recomendSort = recomendIsotope.getAttribute('data-recomend-sort') ? recomendIsotope.getAttribute('data-recomend-sort') : 'original-order';
-
-  window.addEventListener('load', () => {
-    let recomendIsotope = new Isotope(document.querySelector('.recomend-container'), {
-      itemSelector: '.recomend-item',
-      layoutMode: recomendLayout,
-      filter: recomendFilter,
-      sortBy: recomendSort
-    });
-
-    let menuFilters = document.querySelectorAll('.recomend-isotope .recomend-flters li');
-    menuFilters.forEach(function(el) {
-      el.addEventListener('click', function() {
-        document.querySelector('.recomend-isotope .recomend-flters .filter-active').classList.remove('filter-active');
-        this.classList.add('filter-active');
-        recomendIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        if (typeof aos_init === 'function') {
-          aos_init();
-        }
-      }, false);
-    });
-
-  });
-
-}
     
 })(jQuery);
